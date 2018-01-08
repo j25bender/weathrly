e2baebc7125af9f4c56a89560616268af716d26e
Jeff Bender <j25bender@gmail.com>
2018-01-08 14:13:25 -0700

    test

---
+++ test/App.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import App from "../lib/App";
+import cleanData from "../lib/cleanData";
+import mockData from "../lib/mockData";
+import localStorageMock from '../__mocks__/localStorage.js';
+
+describe("App", () => {
+  let app;
+
+  beforeEach(() => {
+    localStorageMock.setItem('40201')
+    app = mount(<App />);
+  });
+  console.log(localStorageMock.setItem)
+  
+  it("App should exist", () => {
+    expect(app).toBeDefined();
+  });
+
+  it("App should mount <Search />", () => {
+    expect(app.find("Search")).toBeDefined();
+    expect(app.find("Search").length).toEqual(1);
+    expect(App).toMatchSnapshot();
+  });
+
+  it("should be a function", () => {
+    expect(typeof app.state).toEqual("function");
+  });
+
+  it("should render the weather page when a city is specified", () => {
+    let filteredData = cleanData(mockData)
+    app.setState({
+        CurrentWeather: filteredData.CurrentObject,
+        SevenHourForecast: filteredData.sevenHourForecastArray,
+        TenDayForecast: filteredData.tenDayArray
+    })    
+
+    expect(app.find("Search")).toBeDefined();
+    expect(app.find("Search").length).toEqual(1);
+
+    expect(app.find("CurrentWeather")).toBeDefined();
+    expect(app.find("CurrentWeather").length).toEqual(1);
+
+    // expect(app.find("TenHour")).toBeDefined();
+    // expect(app.find("TenHour").length).toEqual(1);
+
+    // expect(app.find("SevenDay")).toBeDefined();
+    // expect(app.find("SevenDay").length).toEqual(1);
+
+    // expect(App).toMatchSnapshot();    
+  });
+});
